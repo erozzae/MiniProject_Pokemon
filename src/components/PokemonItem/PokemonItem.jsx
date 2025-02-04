36edfc7af4098e734ec0f86b35b39a9bffb9e3fb
erozzae <rosihann14@mail.ugm.ac.id>
2025-02-04 23:05:43 +0700

    styling layout list pokemon

---
+++ src/components/PokemonItem/PokemonItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PokemonItem() {
+  return (
+    <div>PokemonItem</div>
+  )
+}
+
+export default PokemonItem
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -5,11 +5,11 @@ function PokemonList() {
   const [pokemons] = useState(pokemonJSON);
   return (
     <div>
-        {
-            pokemons.map(item => (
-                <div>{item.name}</div>
-            ))
-        }
+      <div className="list-pokemon d-flex flex-wrap justify-content-center gap-3 my-3">
+        {pokemons.map((item) => (
+          <h1>{item.name}</h1>
+        ))}
+      </div>
     </div>
   );
 }
